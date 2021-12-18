b16ebc1684638792a65e90027460a1c2f94f04dc
cchampou <clement@champouillon.com>
2021-12-18 17:19:02 +0100

    refactor: add log when success and clean models

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Scan;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class FoodController extends Controller
 {
     public function getByEan(Request $request, $ean): \Illuminate\Http\JsonResponse
     {
-        $response = Http::get('https://world.openfoodfacts.org/api/v0/product/' . $ean . '.json');
+        $response = Http::get('https://world.openfoodfacts.org/api/v0/product/' . $ean);
         $scan = new Scan();
         $scan->ean = $ean;
         $scan->save();
+        Log::info('Saved scan entry');
         return response()->json($response->json());
     }
 }
--- app/Models/Scan.php
+++ app/Models/Scan.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $ean
+ */
 class Scan extends Model
 {
     use HasFactory;
-
-    /**
-     * @var string
-     */
-    private $ean;
 }
