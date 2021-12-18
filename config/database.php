371f11d104da59374d78bbec733052fda6e4e5f4
cchampou <clement@champouillon.com>
2021-12-18 16:14:02 +0100

    chore(db): init scan model, clean and update migrations

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('postgres://oqryhxef:qLUFH65jJ0fmac9vn_rI8J1pDvc3HQNe@tyke.db.elephantsql.com/oqryhxef'),
+            'host' => env('DB_HOST', 'tyke.db.elephantsql.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'oqryhxef'),
+            'username' => env('DB_USERNAME', 'oqryhxef'),
+            'password' => env('DB_PASSWORD', 'qLUFH65jJ0fmac9vn_rI8J1pDvc3HQNe'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
